afc243946fe25e2873519d888c28ad0f413b754f
evilsunny <alinok@e-mail.ua>
2014-10-15 14:28:32 +0300

    new Gauss

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	var colRow = 3;
 
 		
-		 
+		 $("#res").hide();
+		 $("#end").hide();
 
 	function readInput() {
 		
@@ -24,6 +25,7 @@ $(document).ready(function(){
 	$('#submitBtn').on('click', function(){
 
 		$('#end').empty();
+		$('#end').show();
         $('#end').append("Step by Step Solutions");
         $('#res').show();
 		   var A = writeToArray(readInput());
@@ -175,39 +177,56 @@ $(document).ready(function(){
        var e = 0.0001;
         var n = matrix.length;
         var matrix1 =matrix;
-        //g.show(g.matrix);
-        for (var k = 0; k < n - 1; k++){
-            var k1 = k;
-            while (matrix[k][k] == 0){
-                k1++;
-                if (k1 == n) {
-                	$('#end').empty();
+        
+        for (var k = 0; k < n ; k++){
+            var aa = Math.abs(matrix[k][k]);
+            var i = k;
+
+            for (var m = k+1; m < n; m++) {
+            	if(Math.abs(matrix[m][k]>aa)){
+            		i=m;
+            		aa = Math.abs(matrix[m][k]);
+            	}
+            };
+
+            if (aa === 0){
+
+            		$('#end').empty();
                 	 $('#end').append("Your matrix is empty");
                 	 $('#results').hide();
                 	 $('#res').hide();
                     return -1;
-
-                    }
-                var r = matrix[k1];
-                matrix[k1] = matrix[k];
-                matrix[k] = r;                
-            
-                    createResult(matrix);
-                
-        
-                }
-            for (var i = k + 1; i < n; i++){
-                if (matrix[i][k] != 0) {
-                    var m1 = - matrix[i][k] / matrix[k][k];
-                    for (var j = k; j <= n; j++){
-                        matrix[i][j] = +matrix[i][j] + matrix[k][j] * m1;
-                        }
-
-                     createResult(matrix);
-
-                    }
-                }
             }
+
+
+            if (i!=k){
+
+            	for (var j = k; j < n+1; j++) {
+            		var bb = matrix[k][j];
+            		matrix[k][j] = matrix[i][j];
+            		matrix[i][j] = bb;
+            	};
+            }
+
+            aa = matrix[k][k];
+            matrix[k][k] = 1;
+
+            for (var j = k+1; j < n+1; j++) {
+            	matrix[k][j] = matrix[k][j]/aa;
+            };
+
+            for (var i = k+1; i < n; i++) {
+            	bb = matrix[i][k];
+            	matrix[i][k] = 0;
+            	if(bb!==0){
+            		for (var j = k+1; j < n+1; j++) {
+            			matrix[i][j] = matrix[i][j] - bb*matrix[k][j];
+            		};
+            	}
+            };
+            createResult(matrix);
+        }
+           
 
             x[n - 1] = Math.round((matrix[n - 1][n] / matrix[n - 1][n - 1]) / e) * e;
 
